5001a22805f7b5b177aea1a09b669504ea6ec60a
RazanAldhafian <98522684+RazanAldhafian@users.noreply.github.com>
2022-05-18 12:51:34 +0300

    Update Insert-Reviews.php

--- Insert-Reviews.php
+++ Insert-Reviews.php
@@ -5,18 +5,18 @@ include 'db-connect.php'
 $errors = array(); //To store errors
 $form_data = array(); //Pass back the data to `form.php`
  
-    $userName = $_POST['username'];
-    $comments = $_POST['comments'];
+    $name = $_POST['username'];
+    $review = $_POST['comments'];
     $placeId = $_POST['placeId'];
     $date=date('Y-m-d H:i:s');
     $star= $_POST['star'];;
    
-    
-
    $insertStr ="INSERT INTO `reviews` (`comments`, `date`, `place_id`, `star`, `username`)
-   VALUES ('".$comments."', '".$date."', '".$placeId."', '".$star."', '".$userName."')";
-   $response= mysqli_query($connection ,$insertStr);
+   VALUES ('".$review."', '".$date."', '".$placeId."', '".$star."', '".$name."')";
+   $response= mysqli_query($connection ,$insertStr) or die(mysqli_error($connection));
+   //$response= mysqli_query($connection ,$insertStr) or die(mysqli_error($connection));
+
    $request_id= mysqli_insert_id($connection);
 
-   header("Location:reviewss.php?id=".$placeId);
+  header("Location:reviewss.php?id=".$placeId);
 ?>
